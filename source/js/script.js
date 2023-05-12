dd70e495b6bf7da15c8f14cae43ad061ca6efb14
Dmitry T <92183078+demetr1ss@users.noreply.github.com>
2023-05-13 00:14:54 +0300

    fix: уберет затемнение + пофиксит заголовок

--- source/js/script.js
+++ source/js/script.js
@@ -52,22 +52,3 @@ window.addEventListener('scroll', () => {
     button.addEventListener('click', scrollToSkills);
   }
 });
-
-function onEntry(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-     change.target.classList.add('element-show');
-    } else {
-      change.target.classList.remove('element-show');
-    }
-  });
-}
-
-let options = {
-  threshold: [0.5] };
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.projects-list__item');
-
-for (let elm of elements) {
-  observer.observe(elm);
-}
