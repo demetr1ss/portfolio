32d97ce95b97b2c072e91832184b1156430f7a50
Dmitry T <92183078+demetr1ss@users.noreply.github.com>
2023-05-30 10:33:32 +0300

    refactor with GPT3.5

--- source/js/script.js
+++ source/js/script.js
@@ -1,25 +1,15 @@
-'use strict';
-
-const button = document.querySelector('.page-hero__link');
 const skillsContainer = document.querySelector('.skills');
 const projectsContainer = document.querySelector('.projects');
 const footerSocialContainer = document.querySelector('.footer__socilal');
 
-const scrollToSkills = function () {
+const scrollTo = (target) => {
   window.scrollTo({
-    top: skillsContainer.offsetTop,
+    top: target.offsetTop,
     behavior: 'smooth',
   });
 };
 
-const scrollToTop = function () {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-};
-
-function inViewport(element) {
+const inViewport = (element) => {
   if (!element) return false;
   if (1 !== element.nodeType) return false;
 
@@ -27,28 +17,35 @@ function inViewport(element) {
   const rect = element.getBoundingClientRect();
 
   return !!rect && rect.bottom >= 0 && rect.right >= 0 && rect.left <= html.clientWidth && rect.top <= html.clientHeight;
-}
+};
 
-button.addEventListener('click', scrollToSkills);
+const hideButtonClass = 'page-hero__link--hide';
+const rotateButtonClass = 'page-hero__link--rotate';
 
-window.addEventListener('scroll', () => {
+const scrollHandler = () => {
   const scrollY = window.scrollY || document.documentElement.scrollTop;
 
   if (scrollY < 100 || inViewport(footerSocialContainer)) {
-    button.classList.remove('page-hero__link--hide');
+    button.classList.remove(hideButtonClass);
   }
 
   if (scrollY > 100 && !inViewport(footerSocialContainer)) {
-    button.classList.add('page-hero__link--hide');
+    button.classList.add(hideButtonClass);
   }
 
   if (inViewport(footerSocialContainer)) {
-    button.classList.add('page-hero__link--rotate');
-    button.removeEventListener('click', scrollToSkills);
-    button.addEventListener('click', scrollToTop);
+    button.classList.add(rotateButtonClass);
+    button.removeEventListener('click', handleClick);
+    button.addEventListener('click', () => scrollTo(document.body));
   } else {
-    button.classList.remove('page-hero__link--rotate');
-    button.removeEventListener('click', scrollToTop);
-    button.addEventListener('click', scrollToSkills);
+    button.classList.remove(rotateButtonClass);
+    button.removeEventListener('click', () => scrollTo(document.body));
+    button.addEventListener('click', handleClick);
   }
-});
+};
+
+const button = document.querySelector('.page-hero__link');
+const handleClick = () => scrollTo(skillsContainer);
+button.addEventListener('click', handleClick);
+
+window.addEventListener('scroll', scrollHandler);
